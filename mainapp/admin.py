48946a77750f76c815f632708cdc36de214a63fa
Roman-Ch-32 <nod3223@ya.ru>
2021-07-25 01:44:24 +0300

    3

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import ProductCategory
+from mainapp.models import Product
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
