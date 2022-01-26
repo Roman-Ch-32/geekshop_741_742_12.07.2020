131f40a740e35819a7f3097e1c6d3406165f6f2a
Roman-Ch-32 <nod3223@ya.ru>
2022-01-26 19:45:24 +0300

    dgango 2 lesson 4

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
 from authapp.models import ShopUser
+from basketapp.admin import BasketAdmin
+from basketapp.models import Basket
 
-admin.site.register(ShopUser)
+
+@admin.register(ShopUser)
+class UserAdmin(admin.ModelAdmin):
+    model = Basket
+    inlines = (BasketAdmin,)
+
+#admin.site.register(ShopUser)
--- basketapp/models.py
+++ basketapp/models.py
@@ -1,27 +1,63 @@
 from django.db import models
 
+from authapp.models import ShopUser
 from geekshop import settings
 from mainapp.models import Product
 
 
+class BasketQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for item in self:
+            item.product.quantity += item.quantity
+            item.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
+objects = BasketQuerySet.as_manager()
+
+
 class Basket(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='колличество', default=0)
     add_datetime = models.DateTimeField(verbose_name="время добаввления", auto_now_add=True)
+    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-    @property
+
+    def __str__(self):
+        return f'Корзина для  {self.user.username} | Продукт{self.product.name}'
+
+
+    def get_baskets(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return baskets
+
+    def sum(self):
+        return self.quantity * self.product.price
+
+    def total_sum(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.sum() for basket in baskets)
+
     def total_quantity(self):
-        items = Basket.objects.filter(user=self.user)
-        total_quantity = sum(list(map(lambda x: x.quantity, items)))
-        return total_quantity
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.quantity for basket in baskets)
 
-    @property
-    def product_cost(self):
-        return self.product.price * self.quantity
+    def delete(self, *args, **kwargs):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(Basket, self).delete(*args, **kwargs)
 
-    @property
-    def total_cost(self):
-        items = Basket.objects.filter(user=self.user)
-        total_cost = sum(list(map(lambda x: x.product_cost, items)))
-        return total_cost
+    def save(self, *args, **kwargs):
+        if self.pk:
+            get_item = self.get_item(int(self.pk))
+            self.product.quantity -= (self.quantity - get_item)
+        else:
+            self.product.quantity -= self.quantity
+
+        self.product.save()
+        super(Basket, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
